7b749edc034c8c3c56c33b30252d5869fcaa11ca
Shraddha-V22 <shraddhav1935@gmail.com>
2023-05-17 20:44:36 +0530

    create wishlist page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { useWishlist } from "../contexts/WishlistProvider";
+import Product from "../components/Product";
 
 export default function Wishlist() {
-  return <div>Wishlist</div>;
+  const { wishlist } = useWishlist();
+  return (
+    <section className="mx-auto mb-8 w-[fit-content]">
+      {wishlist.length > 0 ? (
+        <section className="flex max-w-[850px] flex-wrap gap-8">
+          {wishlist.map((item) => (
+            <Product key={item.id} item={item} />
+          ))}
+        </section>
+      ) : (
+        <p>Add products to wishlist</p>
+      )}
+    </section>
+  );
 }
