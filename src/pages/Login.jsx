1889ed68256fe18896437e5b8f34f54fa453f34d
Shraddha-V22 <shraddhav1935@gmail.com>
2023-05-18 21:51:50 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [loginCreds, setLoginCreds] = useState({ email: "", password: "" });
+
+  const handleLoginCreds = (e) => {
+    const { name, value } = e.target;
+    setLoginCreds((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const loginHandler = async () => {
+    console.log(loginCreds);
+    const request = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(loginCreds),
+    });
+
+    let res = await request.json();
+    console.log(res);
+  };
+
   return (
     <section className="grid h-[100vh] w-full place-items-center">
       <article className="flex max-w-[350px] flex-col gap-8 rounded-md border-[1px] border-[#2C74B3]/20 p-8">
@@ -11,13 +31,20 @@ export default function Login() {
             className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
             type="email"
             placeholder="Email"
+            name="email"
+            onChange={handleLoginCreds}
           />
           <input
             className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
             type="password"
             placeholder="Password"
+            name="password"
+            onChange={handleLoginCreds}
           />
-          <button className="rounded-md border-[1px] border-[#2C74B3]/20 p-2">
+          <button
+            onClick={loginHandler}
+            className="rounded-md border-[1px] border-[#2C74B3]/20 p-2"
+          >
             Sign in
           </button>
         </div>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Registration() {
+  const [signUpCreds, setSignUpCreds] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignUpCreds = (e) => {
+    const { name, value } = e.target;
+    setSignUpCreds((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const signUpHandler = async () => {
+    console.log(signUpCreds);
+    const request = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(signUpCreds),
+    });
+
+    const res = await request.json();
+    console.log(res);
+  };
+
   return (
     <section className="grid h-[100vh] w-full place-items-center">
       <article className="flex w-[350px] flex-col gap-8 rounded-md border-[1px] border-[#2C74B3]/20 p-8">
@@ -10,19 +35,35 @@ export default function Registration() {
           <input
             className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
             type="text"
-            placeholder="Full Name"
+            placeholder="Firstname"
+            name="firstName"
+            onChange={handleSignUpCreds}
+          />
+          <input
+            className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
+            type="text"
+            placeholder="Lastname"
+            name="lastName"
+            onChange={handleSignUpCreds}
           />
           <input
             className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
             type="email"
             placeholder="Email"
+            name="email"
+            onChange={handleSignUpCreds}
           />
           <input
             className="rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
             type="password"
             placeholder="Password"
+            name="password"
+            onChange={handleSignUpCreds}
           />
-          <button className="rounded-md border-[1px] border-[#2C74B3]/20 p-2">
+          <button
+            onClick={signUpHandler}
+            className="rounded-md border-[1px] border-[#2C74B3]/20 p-2"
+          >
             Sign in
           </button>
         </div>
