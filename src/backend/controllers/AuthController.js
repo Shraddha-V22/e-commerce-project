4c5f4d7cd48e0b5f5e49863754175339a9ebd603
Shraddha-V22 <shraddhav1935@gmail.com>
2023-05-18 18:25:31 +0530

    add env variable

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -40,7 +40,7 @@ export const signupHandler = function (schema, request) {
       wishlist: [],
     };
     const createdUser = schema.users.create(newUser);
-    const encodedToken = sign({ _id, email }, process.env.REACT_APP_JWT_SECRET);
+    const encodedToken = sign({ _id, email }, import.meta.env.VITE_JWT_SECRET);
     return new Response(201, {}, { createdUser, encodedToken });
   } catch (error) {
     return new Response(
@@ -73,7 +73,7 @@ export const loginHandler = function (schema, request) {
     if (password === foundUser.password) {
       const encodedToken = sign(
         { _id: foundUser._id, email },
-        process.env.REACT_APP_JWT_SECRET
+        import.meta.env.VITE_JWT_SECRET
       );
       foundUser.password = undefined;
       return new Response(200, {}, { foundUser, encodedToken });
--- src/backend/utils/authUtils.js
+++ src/backend/utils/authUtils.js
@@ -6,7 +6,7 @@ export const requiresAuth = function (request) {
   const encodedToken = request.requestHeaders.authorization;
   const decodedToken = jwt_decode(
     encodedToken,
-    process.env.REACT_APP_JWT_SECRET
+    import.meta.env.VITE_JWT_SECRET
   );
   if (decodedToken) {
     const user = this.db.users.findBy({ email: decodedToken.email });
