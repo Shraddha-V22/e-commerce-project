ed9b7aadde97cfff5ff70015d9cc548cd41e0d00
Shraddha-V22 <shraddhav1935@gmail.com>
2023-05-24 09:59:16 +0530

    add toast component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout() {
   return (
-    <section className="relative bg-[#E3F2C1]/20 font-nanumGothic">
+    <section className="relative grid grid-rows-[auto_1fr_auto] bg-[#E3F2C1]/20 font-nanumGothic">
       <Header />
       <main className="pt-[100px]">
         <Outlet />
+        <ToastContainer className="h-[50px] w-[200px]" />
       </main>
       <Footer />
     </section>
