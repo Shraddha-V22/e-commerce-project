31cca72f8cc563a1f1fcfa4c63d0e9ab44a2e160
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-04 15:49:02 +0530

    implement scroll to top

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,10 +4,27 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    gridRef.current.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
   return (
-    <section className="relative grid h-[100vh] grid-rows-[1fr_auto] overflow-x-hidden font-nanumGothic">
+    <section
+      className="relative grid h-[100vh] grid-rows-[1fr_auto] overflow-x-hidden font-nanumGothic"
+      ref={gridRef}
+    >
       <Header />
       <main className="bg-[#E3F2C1]/20 pt-[100px]">
         <Outlet />
