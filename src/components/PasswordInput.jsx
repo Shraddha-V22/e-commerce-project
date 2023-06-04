fc80d1e3f7f4abb043ff9f8e91e4e8244674fe3f
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-04 22:03:41 +0530

    update

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -4,7 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-export default function PasswordInput({ onChangeHandler }) {
+export default function PasswordInput({
+  onChangeHandler,
+  placeholder = "Password",
+  name = "password",
+}) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
@@ -13,16 +17,16 @@ export default function PasswordInput({ onChangeHandler }) {
         <input
           className="w-full rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
           type="password"
-          placeholder="Password"
-          name="password"
+          placeholder={placeholder}
+          name={name}
           onChange={onChangeHandler}
         />
       ) : (
         <input
           className="w-full rounded-md border-[1px] border-[#2C74B3]/20 p-2 outline-none"
           type="text"
-          placeholder="Password"
-          name="password"
+          placeholder={placeholder}
+          name={name}
           onChange={onChangeHandler}
         />
       )}
