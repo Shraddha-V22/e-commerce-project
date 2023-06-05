fe434dfe3c11f88b967c6b0ff74023f3465edb77
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-05 15:53:34 +0530

    add type prop

--- src/components/DetailsInput.jsx
+++ src/components/DetailsInput.jsx
@@ -1,8 +1,14 @@
-export function DetailsInput({ placeholder, name, onChange, value }) {
+export function DetailsInput({
+  placeholder,
+  name,
+  onChange,
+  value,
+  type = "text",
+}) {
   return (
     <input
       className="border-b-[1px] indent-1 outline-none placeholder:text-sm"
-      type="text"
+      type={type}
       value={value}
       placeholder={placeholder}
       name={name}
